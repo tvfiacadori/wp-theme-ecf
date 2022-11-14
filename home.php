b20e84ce01a69d31d8724d08c1c9b73d84d23712
Talita Fiacadori <tvfiacadori@gmail.com>
2022-11-14 14:42:53 +0100

    update BLOG

--- home.php
+++ home.php
@@ -1,12 +1,27 @@
-<?php get_header(); ?>
-<div class="container-post">
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <div class="post">
-        <?php the_content(); ?>
-        <h1 class="h1"><?php the_title(); ?></h1>
-        <?php the_date(); ?><?php the_category(); ?>
-      </div>
-  <?php endwhile;
-  endif; ?>
-</div>
-<?php get_footer(); ?>
+<?php
+get_header();
+?>
+
+<div class="page">
+  <div class="section_articles">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <article class="post">
+          <?php the_post_thumbnail('medium', ['class' => 'card-img-top', 'alt' => '']); ?>
+          <div class="post__meta">
+            <p class="post_date"><?php the_time(get_option('date_format')); ?></p>
+            <p class="post_category"><?php the_category(); ?> </p>
+          </div>
+          <h5><?php the_title(); ?></h5>
+          <p class="excerpt"><?php the_excerpt(); ?></p>
+          <p>
+            <a href="<?php the_permalink(); ?>" class="post__link">Read More</a>
+          </p>
+        </article>
+    <?php endwhile;
+    endif; ?>
+  </div>
+
+
+<?php
+get_footer();
+?>
